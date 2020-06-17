ca2def0b9c52d8326d4d12409b0a2b14b321858f
Atish Maharjan <arcrt1993@gmail.com>
2020-06-17 14:42:05 +0545

    displayed all pokemons in Card view with images

--- src/components/pokecard/pokecard.js
+++ src/components/pokecard/pokecard.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import './pokecard.css';
+import imgNA from '../../img/image-not-available.jpg';
+import Tilt from 'react-tilt';
 
 //const POKE_API ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-const IMG_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+//fancier image API
+const IMG_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
 //RULE: padding id to 3 digits number to fetch better pokemon images
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3):number);
 
-export default function Pokecard({id, name}) {
+export default function Pokecard({name, url}) {
 
-    let imgSrc = `${IMG_API}${padToThree(id)}.png`; 
+    const splitUrl = url.split('/');
+    const lastURLSegment = splitUrl.pop() || splitUrl.pop();  // handle potential trailing slash
+    
+    const pokemon_id=padToThree(lastURLSegment);
+
+    const imgSource = `${IMG_API}${padToThree(pokemon_id)}.png`; 
 
     return (
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }}>
         <div className="pokecard">
-            
-            <img src={imgSrc} alt=""/>
-            <h1 className="pokecard-title">{name}</h1>
+            <img src={imgSource ? imgSource : imgNA} title="click for details" alt=""/>
+            <div className='description'>
+            <h4 className="pokecard-title" >{pokemon_id}</h4>
+            <h4 className="pokecard-title" >{name}</h4>
+            </div>
         </div>
+        </Tilt>
     )
 }
-
-
-//{id: 4, name:'Charmander', type:'fire', base_experience: 62}
--- src/components/pokedex/pokedex.js
+++ src/components/pokedex/pokedex.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import Pokecard from '../pokecard/pokecard.js';
+import axios from 'axios';
 import './pokedex.css';
 
-export default function Pokedex() {
+class Pokedex extends React.Component{
 
-    let pokemon = [
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-        {id: 4, name:'Charmander', type:'fire', base_experience: 62},
-    ];
 
-    return (
-        <div className="pokedex">
-            <h1>Pokedex !</h1>
-            <div className="pokedex-cards">
-                {pokemon.map((p)=>(
-                    <Pokecard key={p.id} id={p.id} name={p.name}/>
-                ))}
+    state={
+        poke_api:"https://pokeapi.co/api/v2/pokemon?limit=964",
+        pokemon:null
+    }
+
+    async componentDidMount(){
+        const res = await axios.get(this.state.poke_api);
+        this.setState({pokemon: res.data.results});
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+            {this.state.pokemon ? (
+            <div className="pokedex">
+                <h1>Pokedex !</h1>
+                <div className="pokedex-cards">
+                    {this.state.pokemon.map((p)=>(
+                        <Pokecard key={p.name} name={p.name} url={p.url}/>
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+            ) : (('Loading...')) }
+            </React.Fragment>
+        )
+    }
 }
+export default Pokedex;
